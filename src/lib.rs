a7be6761704f72cffb5d2fe34b52e4a8f4626571
devanbenz <devandbenz@gmail.com>
2026-01-31 10:38:07 -0600

    feat: Adding serialization to AppendEntries

--- src/lib.rs
+++ src/lib.rs
@@ -25,18 +25,47 @@ impl Log {
 #[derive(Debug)]
 pub(crate) struct ApppendEntries {
     term: u64,
+    leader_id: u64,
+    prev_log_index: u64,
+    prev_log_term: u64,
+    leader_commit: u64,
+    entries: Vec<LogEntry>,
 }
 
 impl ApppendEntries {
     pub fn new() -> Self {
-        Self {}
+        Self {
+            term: 0,
+            leader_id: 0,
+            prev_log_index: 0,
+            prev_log_term: 0,
+            leader_commit: 0,
+            entries: vec![],
+        }
     }
 
     pub fn serialize(&self) {
         let mut builder = ::capnp::message::Builder::new_default();
-        let append_entries = builder.init_root::<append_entries_request::Builder>();
-        append_entries.set_term(value);
-        append_entries.init_entries()
+        let mut append_entries = builder.init_root::<append_entries_request::Builder>();
+        append_entries.set_term(self.term);
+        append_entries.set_leader_id(self.leader_id);
+        append_entries.set_prev_log_index(self.prev_log_index);
+        append_entries.set_prev_log_term(self.prev_log_term);
+        append_entries.set_leader_commit(self.leader_commit);
+        let mut entries = append_entries.init_entries(self.entries.len() as u32);
+
+        for (i, entry) in self.entries.iter().enumerate() {
+            let mut v = entries
+                .reborrow()
+                .get(i as u32)
+                .expect("get entries buffer");
+            let s = match str::from_utf8(&entry.command) {
+                Ok(v) => v,
+                Err(e) => panic!("Invalid UTF-8: {}", e),
+            };
+
+            v.push_str(s);
+        }
     }
 }
 
